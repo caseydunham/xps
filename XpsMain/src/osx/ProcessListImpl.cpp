24566444ef26743249cb27f802432b06f2eb5c48
Casey Dunham <casey.dunham@gmail.com>
2013-05-16 20:37:51 -0400

    initial osx placeholder implementation

---
+++ XpsMain/src/osx/ProcessListImpl.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+
+#include "Process.h"
+#include "ProcessList.h"
+
+std::vector<Process> ProcessList::scan() {
+  std::vector<Process> procs;
+  return procs;
+}
